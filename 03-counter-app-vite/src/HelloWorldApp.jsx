624409909a45f682a833af43605d8e804e2758d1
hspdaw <hspdaw@gmail.com>
2023-02-06 21:35:37 +0100

    End CounterApp

---
+++ 03-counter-app-vite/src/HelloWorldApp.jsx
@@ -0,0 +1,9 @@
+export const HelloWorldApp = () => {
+
+
+    return (
+      <h1>Hello World App !!!</h1>
+    );
+  }
+  
+  
--- 03-counter-app-vite/src/main.jsx
+++ 03-counter-app-vite/src/main.jsx
@@ -1,10 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { CounterApp } from './CounterApp';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+import './style.css';
+
+
+
+ReactDOM.createRoot( document.getElementById('root') ).render(
+    <React.StrictMode>
+        <CounterApp value={ 20 } />
+
+    </React.StrictMode>
+);
+
