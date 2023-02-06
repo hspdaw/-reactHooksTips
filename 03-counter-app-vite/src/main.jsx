c6fbe679931d67d871621d3e4b299d6f17f96044
hspdaw <hspdaw@gmail.com>
2023-02-06 21:43:18 +0100

    Remover archivos que no se utilizan

--- 03-counter-app-vite/src/main.jsx
+++ 03-counter-app-vite/src/main.jsx
@@ -5,11 +5,9 @@ import { CounterApp } from './CounterApp';
 import './style.css';
 
 
-
 ReactDOM.createRoot( document.getElementById('root') ).render(
     <React.StrictMode>
         <CounterApp value={ 20 } />
-
     </React.StrictMode>
 );
 
