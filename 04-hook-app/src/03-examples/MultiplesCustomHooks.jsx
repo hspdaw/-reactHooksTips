3fe194bfff6991327537d8b45ee17b50338f1236
hspdaw <hspdaw@gmail.com>
2023-02-15 21:36:36 +0100

    Hooks useFetch + useCounter

--- 04-hook-app/src/03-examples/MultiplesCustomHooks.jsx
+++ 04-hook-app/src/03-examples/MultiplesCustomHooks.jsx
@@ -1,15 +1,35 @@
 
 
 import { useFetch } from "../hooks/useFetch"
+import {useCounter } from "../hooks/useCounter"
 
 export const MultiplesCustomHooks = () => {
+  const { counter , increment } = useCounter(1);
+  const { data, isLoading, hasError } = useFetch( `https://api.breakingbadquotes.xyz/v1/quotes/${counter}`);
+  //console.log({data, isLoading, hasError});
+                            //si tiene valor entonces (doble negación)
+  const { author, quote } = !!data && data[0];
+  /* if( isLoading ){
+    return (<h1>Loading</h1>)
+  } */
 
-  const { data, isLoading, hasError } = useFetch('https://api.breakingbadquotes.xyz/v1/quotes/1');
-  console.log({data, isLoading, hasError});
   return (
     <>
       <h1>BreakingBad Quotes</h1>
       <hr />
+      {isLoading
+        ?
+        (<div className="alert alert-info text-center">
+          Loading...
+        </div>)
+        : (<blockquote className="blockquote text-end">
+          <p className="mb-1">{quote}</p>
+          <footer className="blockquote-footer">{author}</footer>
+        </blockquote>)
+      }
+      <button className="btn btn-primary" disabled={isLoading} onClick={()=>increment()}>
+        Next Quote
+      </button>
     </>
   )
 }
