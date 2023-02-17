8773bdc807a2a1b3559dbdef6e4af312f7fc1fd6
hspdaw <hspdaw@gmail.com>
2023-02-17 21:33:34 +0100

    Delete todo Item

--- 04-hook-app/src/08-useReducer/TodoApp.jsx
+++ 04-hook-app/src/08-useReducer/TodoApp.jsx
@@ -11,7 +11,7 @@ const initialState = [
         } */
 ];
 
-const init = () =>{
+const init = () => {
     return JSON.parse(localStorage.getItem('todos') || [])
 }
 
@@ -33,13 +33,20 @@ export const TodoApp = () => {
         dispatch(action);
     }
 
+    const handleDeleteTodo = (id) => {
+        dispatch({
+            type: '[TODO] Remove Todo',
+            payload: id
+        });
+    }
+
     return (
         <>
             <h1>TodoApp 10 <small>Pendientes: 2</small></h1>
             <hr />
             <div className="row">
                 <div className="col-7">
-                    <TodoList todos={todos} />
+                    <TodoList todos={todos} onDeleteTodo={handleDeleteTodo} />
                 </div>
                 <div className="col-5">
                     <TodoForm onNewTodo={handleNewTodo} />
