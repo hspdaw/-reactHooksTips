bb5d2b1947e5317762a9507dff6e1c1f808b382f
hspdaw <hspdaw@gmail.com>
2023-02-23 22:11:45 +0100

    Test Simular evento onclick

--- 03-counter-app-vite/tests/CounterApp.test.jsx
+++ 03-counter-app-vite/tests/CounterApp.test.jsx
@@ -1,19 +1,42 @@
 import { CounterApp } from '../src/CounterApp';
-import { render, screen } from '@testing-library/react';
+import { fireEvent, getByRole, render, screen } from '@testing-library/react';
 
 describe('Pruebas en <CounterApp/>', () => {
 
-  const value = 100;
+    const value = 100;
 
-  test('debe hacer match con el snapshot', () => {
-    const { container } = render(<CounterApp  value={value} />);
-    expect(container).toMatchSnapshot();
-  })
+/*     test('debe hacer match con el snapshot', () => {
+        const { container } = render(<CounterApp value={value} />);
+        expect(container).toMatchSnapshot();
+    })
 
-  test('debe mostrar el valor inicial de 100 <CounterApp  value={100} />', () => {
-    render(<CounterApp value={value} />);
-    expect(screen.getByText(value)).toBeTruthy();
-  })
+    test('debe mostrar el valor inicial de 100 <CounterApp  value={100} />', () => {
+        render(<CounterApp value={value} />);
+        expect(screen.getByText(value)).toBeTruthy();
+    })
+
+
+
+    test('debe incrementar con el botón + 1', () => {
+        //inicializar el sujeto de pruebas
+        render(<CounterApp value={value} />);
+        fireEvent.click(screen.getByText('+1'));
+        //fireEvent.click(screen.getByText('-1'));
+        expect(screen.getAllByText('101')).toBeTruthy();
+       // expect(screen.getAllByText('99')).toBeTruthy();
+    }) */
+
+
+    test('debe funcionar el botón de reset', () => {
+        render(<CounterApp value={value} />);
+        fireEvent.click(screen.getByText('+1'));
+        fireEvent.click(screen.getByText('+1'));
+        fireEvent.click(screen.getByText('+1'));
+        fireEvent.click(screen.getByText('Reset'));     
+        //Otra forma de acceder al objeto fireEvent.click(screen,getByRole('button', {name:'btn-reset'}))
+        expect(screen.getAllByText('100')).toBeTruthy();
+
+    })
 
 
 })
