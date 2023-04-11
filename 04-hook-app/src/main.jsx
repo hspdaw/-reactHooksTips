f483189cb04ce6417ecd9364644a325a7ca06a34
hspdaw <hspdaw@gmail.com>
2023-04-11 22:18:43 +0200

    Fix React-Router-Dom

--- 04-hook-app/src/main.jsx
+++ 04-hook-app/src/main.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
-import ReactDOM from 'react-dom/client'
+//import ReactDOM from 'react-dom/client'
+import { createRoot } from "react-dom/client";
 import {
-    createBrowserRouter,
-    RouterProvider,
-  } from 'react-router'
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Link,
+} from "react-router-dom";
 import './index.css'
 /* 
 import { Padre } from './07-tarea-memo/Padre'
@@ -24,19 +27,18 @@ import { HooksApp } from './HooksApp'
 
 import './08-useReducer/intro-reducer';
 import { TodoApp } from './08-useReducer/TodoApp';
+import { MainApp } from './09-useContext/MainApp';
 
 const router = createBrowserRouter([
     {
       path: "/",
-      element: <TodoApp/>,
+      element: <MainApp/>,
     },
   ]);
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-    <React.StrictMode>
+  createRoot(document.getElementById("root")).render(
     <RouterProvider router={router} />
-    </React.StrictMode>
-)
+  );
 
 /* <React.StrictMode> */
 
