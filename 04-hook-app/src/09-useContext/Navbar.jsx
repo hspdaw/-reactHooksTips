fffc23149c6d6d9bb2f37495783e80b595791366
hspdaw <hspdaw@gmail.com>
2023-05-16 20:18:52 +0200

    Añadir NavLink

--- 04-hook-app/src/09-useContext/Navbar.jsx
+++ 04-hook-app/src/09-useContext/Navbar.jsx
@@ -1,11 +1,38 @@
 
-import { Link } from "react-router-dom"
+import { Link, NavLink } from "react-router-dom"
 export default function Navbar() {
     return (
-        <>
-            <Link to="/">Home</Link>
-            <Link to="/about">About</Link>
-            <Link to="/login">Login</Link>
-        </>
+        /*   <>
+              <Link to="/">Home</Link>
+              <Link to="/about">About</Link>
+              <Link to="/login">Login</Link>
+          </> */
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark rounded-3">
+            <Link className="navbar-brand" href="/">useContext</Link>
+            <div className="collapse navbar-collapse" id="navbarNav">
+                <ul className="navbar-nav">
+
+                    <NavLink
+                        className={({ isActive }) => `nav-link ${isActive ? 'active' : ''}`}
+                        to="/"
+                    >
+                        Home
+                    </NavLink>
+                    <NavLink
+                        className={({ isActive }) => `nav-link ${isActive ? 'active' : ''}`}
+                        to="/about"
+                    >
+                        About
+                    </NavLink>
+                    <NavLink
+                        className={({ isActive }) => `nav-link ${isActive ? 'active' : ''}`}
+                        to="/login"
+                    >
+                        Login
+                    </NavLink>
+
+                </ul>
+            </div>
+        </nav>
     )
 }
